6f955b1bc08e4b52300cbad989a2d43dfb068ba1
Ken Williams <kenwilliams74@gmail.com>
2020-10-08 21:22:06 -0700

    Two main buttons added.

--- client/src/pages/MemberDashboard.js
+++ client/src/pages/MemberDashboard.js
@@ -2,25 +2,45 @@ import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Link } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 
 
 function MemberDashboard (){
 
-  
+  const classes = useStyles();
 
 
     return (
-        
-        <div className = "Profile-settings">
+           <div className={classes.root}>
+             <div className = "Profile-settings">
+      <Grid container spacing={12}>
+      <Button variant="contained" size="large" color="primary" className={classes.margin} component={Link} to="/profilesettings">Profile Settings</Button>
+      </Grid>
+      <Grid container spacing={12}>
+      <Button variant="contained" size="large" color="primary" className={classes.margin} component={Link} to="/profilesettings">Projects</Button>
+      </Grid>
+    </div>
             
-            {/* <img src={'https://pngimg.com/uploads/illuminati/illuminati_PNG1.png'} alt='triangle' height='500px' width='620px'/> */}
-            <Button component={Link} to="/profilesettings" color="inherit">Profile Settings</Button>
-            
-            <Typography>
-              <h1>Member Dashboard</h1>
-          </Typography>
         </div>
        
       )
